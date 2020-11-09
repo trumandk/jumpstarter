b56fd83e5b58c8c57c9fef0ae63cffa0445750d0
root <>
2020-11-09 09:21:52 +0100

    fixed bug in worker stat-app

--- stat/stat.go
+++ stat/stat.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"context"
+	//"context"
 	"encoding/json"
 	"fmt"
 	"github.com/docker/docker/client"
@@ -10,15 +10,12 @@ import (
 	"net/http"
 	"os/exec"
 )
+	//var ctx = context.Background()
+	var cli, errDocker = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 
 func containers(w http.ResponseWriter, req *http.Request) {
 
-	var ctx = context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 
-	if err != nil {
-		panic(err)
-	}
 
 	containerList, err5 := cli.ContainerList(ctx, types.ContainerListOptions{})
 	if err5 != nil {
@@ -33,6 +30,9 @@ func containers(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	if errDocker != nil {
+		panic(errDocker)
+	}
         exec.Command("/bin/mkdir","/var/lib/docker").CombinedOutput()
         out, err2 := exec.Command("/usr/bin/mount","-t","ext4","/dev/sda1","/var/lib/docker").CombinedOutput()
 
